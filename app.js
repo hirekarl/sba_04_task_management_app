1fc60aea32475efb3acf76c71648ed9387a86007
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-06-21 09:42:52 -0400

    Wrapped everything in a 'DOMContentLoaded' eventListener callback.

--- app.js
+++ app.js
@@ -1,223 +1,225 @@
-const taskStatus = Object.freeze({
-  OVERDUE: {
-    value: "overdue",
-    badgeText: "OVERDUE",
-    bsColor: "danger",
-    sortRank: 1,
-  },
-  IN_PROGRESS: {
-    value: "in-progress",
-    badgeText: "IN PROGRESS",
-    bsColor: "warning",
-    sortRank: 2,
-  },
-  NOT_STARTED: {
-    value: "not-started",
-    badgeText: "NOT STARTED",
-    bsColor: "info",
-    sortRank: 3,
-  },
-  COMPLETED: {
-    value: "completed",
-    badgeText: "COMPLETED",
-    bsColor: "success",
-    sortRank: 4,
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  const taskStatus = Object.freeze({
+    OVERDUE: {
+      value: "overdue",
+      badgeText: "OVERDUE",
+      bsColor: "danger",
+      sortRank: 1,
+    },
+    IN_PROGRESS: {
+      value: "in-progress",
+      badgeText: "IN PROGRESS",
+      bsColor: "warning",
+      sortRank: 2,
+    },
+    NOT_STARTED: {
+      value: "not-started",
+      badgeText: "NOT STARTED",
+      bsColor: "info",
+      sortRank: 3,
+    },
+    COMPLETED: {
+      value: "completed",
+      badgeText: "COMPLETED",
+      bsColor: "success",
+      sortRank: 4,
+    },
+  })
+
+  const taskCategory = Object.freeze({
+    HOUSEHOLD: {
+      value: "household",
+      badgeText: "HOUSEHOLD",
+      bsColor: "primary",
+      sortRank: 1,
+    },
+    WORK: {
+      value: "work",
+      badgeText: "WORK",
+      bsColor: "secondary",
+      sortRank: 2,
+    },
+    HOBBY: {
+      value: "hobby",
+      badgeText: "HOBBY",
+      bsColor: "dark",
+      sortRank: 3,
+    },
+    ENTERTAINMENT: {
+      value: "entertainment",
+      badgeText: "ENTERTAINMENT",
+      bsColor: "light",
+      sortRank: 4,
+    },
+  })
+
+  const taskList = {
+    items: [],
+    domElement: document.getElementById("task-list"),
+    addTask: function (task) {
+      this.items.push(task)
+    },
+    removeTask: function (task) {
+      this.items.splice(this.items.indexOf(task), 1)
+    },
+    sort: function () {
+      // TODO
+    },
+    display: function () {
+      this.domElement.innerHTML = ""
+      for (let task of this.items) {
+        const taskListItem = task.createHTML()
+        this.domElement.appendChild(taskListItem)
+      }
+    },
+  }
+
+  function todayDateString() {
+    // YYYY-MM-DD
+    return new Date().toISOString().slice(0, 10)
+  }
+
+  // &nbsp;
+  const nbsp = "\xa0"
+
+  class Task {
+    static nextId = 0
+
+    constructor(name, category, deadline, status) {
+      this.id = Task.nextId++
+      this.htmlId = `task${this.id}`
+      this.name = name
+      this.category = category
+      this.deadline = deadline // YYYY-MM-DD
+      this.status = status
+    }
+
+    setStatus(newStatus) {
+      this.status = newStatus
+    }
+
+    isCompleted() {
+      return this.status === taskStatus.COMPLETED
+    }
+
+    deadlineDateString() {
+      const deadline = new Date(this.deadline)
+      // MM/DD/YYYY
+      return `Due${nbsp}${deadline.toLocaleDateString("en-US")}`
+    }
+
+    setOverdueIfOverdue() {
+      if (!this.isCompleted()) {
+        if (todayDateString() > this.deadline) {
+          this.setStatus(taskStatus.OVERDUE)
+        }
+      }
+    }
+
+    createHTML() {
+      this.setOverdueIfOverdue()
+
+      const taskListItem = document.createElement("li")
+      taskListItem.setAttribute("id", this.htmlId)
+      taskListItem.classList.add(
+        "list-group-item",
+        "d-flex",
+        "justify-content-between",
+        "align-items-start"
+      )
+
+      const div = document.createElement("div")
+      div.classList.add("me-auto")
+      taskListItem.appendChild(div)
+
+      const checkbox = document.createElement("input")
+      checkbox.type = "checkbox"
+      checkbox.classList.add("form-check-input")
+      checkbox.setAttribute("id", `${this.htmlId}-checkbox`)
+      checkbox.setAttribute("aria-label", "Mark completed.")
+      checkbox.addEventListener("change", (event) => {
+        if (event.target.checked) {
+          this.setStatus(taskStatus.COMPLETED)
+          taskList.display()
+        } else {
+          this.setStatus(taskStatus.IN_PROGRESS)
+          this.setOverdueIfOverdue()
+          taskList.display()
+        }
+      })
+      checkbox.checked = this.isCompleted() ? true : false
+      div.appendChild(checkbox)
+
+      const checkboxLabel = document.createElement("label")
+      checkboxLabel.setAttribute("for", `${this.htmlId}-checkbox`)
+      checkboxLabel.classList.add("form-check-label", "fw-bold", "mx-2")
+      if (this.status === taskStatus.OVERDUE) {
+        checkboxLabel.classList.add("text-danger")
+      }
+      checkboxLabel.innerHTML = this.isCompleted()
+        ? `<del>${this.name}</del>`
+        : this.name
+      div.appendChild(checkboxLabel)
+
+      const dueDate = document.createElement("em")
+      dueDate.textContent = this.deadlineDateString()
+      if (this.status === taskStatus.OVERDUE) {
+        dueDate.classList.add("text-danger")
+      }
+      div.appendChild(dueDate)
+
+      const categoryBadge = document.createElement("button")
+      categoryBadge.type = "button"
+      categoryBadge.setAttribute("id", `${this.htmlId}-category-badge`)
+      categoryBadge.classList.add(
+        "btn",
+        "badge",
+        `text-bg-${this.category.bsColor}`,
+        "rounded-pill",
+        "ms-2"
+      )
+      categoryBadge.textContent = this.category.badgeText
+      taskListItem.appendChild(categoryBadge)
+
+      const statusBadge = document.createElement("button")
+      statusBadge.type = "button"
+      statusBadge.setAttribute("id", `${this.htmlId}-status-badge`)
+      statusBadge.classList.add(
+        "btn",
+        "badge",
+        `text-bg-${this.status.bsColor}`,
+        "rounded-pill",
+        "ms-2"
+      )
+      statusBadge.textContent = this.status.badgeText
+      statusBadge.addEventListener("click", (event) => {
+        event.preventDefault()
+        if (this.status === taskStatus.IN_PROGRESS) {
+          this.setStatus(taskStatus.NOT_STARTED)
+          taskList.display()
+        } else if (this.status === taskStatus.NOT_STARTED) {
+          this.setStatus(taskStatus.IN_PROGRESS)
+          taskList.display()
+        }
+      })
+      taskListItem.appendChild(statusBadge)
+
+      const deleteButton = document.createElement("button")
+      deleteButton.type = "button"
+      deleteButton.classList.add("ms-3", "btn-close")
+      deleteButton.setAttribute("aria-label", "Delete.")
+      deleteButton.addEventListener("click", (event) => {
+        event.preventDefault()
+        taskList.removeTask(this)
+        taskList.display()
+      })
+      taskListItem.appendChild(deleteButton)
+
+      if (this.isCompleted()) {
+        taskListItem.classList.add("opacity-50")
+      }
+
+      return taskListItem
+    }
+  }
 })
-
-const taskCategory = Object.freeze({
-  HOUSEHOLD: {
-    value: "household",
-    badgeText: "HOUSEHOLD",
-    bsColor: "primary",
-    sortRank: 1,
-  },
-  WORK: {
-    value: "work",
-    badgeText: "WORK",
-    bsColor: "secondary",
-    sortRank: 2,
-  },
-  HOBBY: {
-    value: "hobby",
-    badgeText: "HOBBY",
-    bsColor: "dark",
-    sortRank: 3,
-  },
-  ENTERTAINMENT: {
-    value: "entertainment",
-    badgeText: "ENTERTAINMENT",
-    bsColor: "light",
-    sortRank: 4,
-  },
-})
-
-const taskList = {
-  items: [],
-  domElement: document.getElementById("task-list"),
-  addTask: function (task) {
-    this.items.push(task)
-  },
-  removeTask: function (task) {
-    this.items.splice(this.items.indexOf(task), 1)
-  },
-  sort: function () {
-    // TODO
-  },
-  display: function () {
-    this.domElement.innerHTML = ""
-    for (let task of this.items) {
-      const taskListItem = task.createHTML()
-      this.domElement.appendChild(taskListItem)
-    }
-  },
-}
-
-function todayDateString() {
-  // YYYY-MM-DD
-  return new Date().toISOString().slice(0, 10)
-}
-
-// &nbsp;
-const nbsp = "\xa0"
-
-class Task {
-  static nextId = 0
-
-  constructor(name, category, deadline, status) {
-    this.id = Task.nextId++
-    this.htmlId = `task${this.id}`
-    this.name = name
-    this.category = category
-    this.deadline = deadline // YYYY-MM-DD
-    this.status = status
-  }
-
-  setStatus(newStatus) {
-    this.status = newStatus
-  }
-
-  isCompleted() {
-    return this.status === taskStatus.COMPLETED
-  }
-
-  deadlineDateString() {
-    const deadline = new Date(this.deadline)
-    // MM/DD/YYYY
-    return `Due${nbsp}${deadline.toLocaleDateString("en-US")}`
-  }
-
-  setOverdueIfOverdue() {
-    if (!this.isCompleted()) {
-      if (todayDateString() > this.deadline) {
-        this.setStatus(taskStatus.OVERDUE)
-      }
-    }
-  }
-
-  createHTML() {
-    this.setOverdueIfOverdue()
-
-    const taskListItem = document.createElement("li")
-    taskListItem.setAttribute("id", this.htmlId)
-    taskListItem.classList.add(
-      "list-group-item",
-      "d-flex",
-      "justify-content-between",
-      "align-items-start"
-    )
-
-    const div = document.createElement("div")
-    div.classList.add("me-auto")
-    taskListItem.appendChild(div)
-
-    const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
-    checkbox.classList.add("form-check-input")
-    checkbox.setAttribute("id", `${this.htmlId}-checkbox`)
-    checkbox.setAttribute("aria-label", "Mark completed.")
-    checkbox.addEventListener("change", (event) => {
-      if (event.target.checked) {
-        this.setStatus(taskStatus.COMPLETED)
-        taskList.display()
-      } else {
-        this.setStatus(taskStatus.IN_PROGRESS)
-        this.setOverdueIfOverdue()
-        taskList.display()
-      }
-    })
-    checkbox.checked = this.isCompleted() ? true : false
-    div.appendChild(checkbox)
-
-    const checkboxLabel = document.createElement("label")
-    checkboxLabel.setAttribute("for", `${this.htmlId}-checkbox`)
-    checkboxLabel.classList.add("form-check-label", "fw-bold", "mx-2")
-    if (this.status === taskStatus.OVERDUE) {
-      checkboxLabel.classList.add("text-danger")
-    }
-    checkboxLabel.innerHTML = this.isCompleted()
-      ? `<del>${this.name}</del>`
-      : this.name
-    div.appendChild(checkboxLabel)
-
-    const dueDate = document.createElement("em")
-    dueDate.textContent = this.deadlineDateString()
-    if (this.status === taskStatus.OVERDUE) {
-      dueDate.classList.add("text-danger")
-    }
-    div.appendChild(dueDate)
-
-    const categoryBadge = document.createElement("button")
-    categoryBadge.type = "button"
-    categoryBadge.setAttribute("id", `${this.htmlId}-category-badge`)
-    categoryBadge.classList.add(
-      "btn",
-      "badge",
-      `text-bg-${this.category.bsColor}`,
-      "rounded-pill",
-      "ms-2"
-    )
-    categoryBadge.textContent = this.category.badgeText
-    taskListItem.appendChild(categoryBadge)
-
-    const statusBadge = document.createElement("button")
-    statusBadge.type = "button"
-    statusBadge.setAttribute("id", `${this.htmlId}-status-badge`)
-    statusBadge.classList.add(
-      "btn",
-      "badge",
-      `text-bg-${this.status.bsColor}`,
-      "rounded-pill",
-      "ms-2"
-    )
-    statusBadge.textContent = this.status.badgeText
-    statusBadge.addEventListener("click", (event) => {
-      event.preventDefault()
-      if (this.status === taskStatus.IN_PROGRESS) {
-        this.setStatus(taskStatus.NOT_STARTED)
-        taskList.display()
-      } else if (this.status === taskStatus.NOT_STARTED) {
-        this.setStatus(taskStatus.IN_PROGRESS)
-        taskList.display()
-      }
-    })
-    taskListItem.appendChild(statusBadge)
-
-    const deleteButton = document.createElement("button")
-    deleteButton.type = "button"
-    deleteButton.classList.add("ms-3", "btn-close")
-    deleteButton.setAttribute("aria-label", "Delete.")
-    deleteButton.addEventListener("click", (event) => {
-      event.preventDefault()
-      taskList.removeTask(this)
-      taskList.display()
-    })
-    taskListItem.appendChild(deleteButton)
-
-    if (this.isCompleted()) {
-      taskListItem.classList.add("opacity-50")
-    }
-
-    return taskListItem
-  }
-}
