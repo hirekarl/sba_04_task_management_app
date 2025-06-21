3400507e5a688c2248d754d597b21035703ff51e
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-06-20 20:59:18 -0400

    Added taskStatus and taskCategory objects.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const taskStatus = {
+  NOT_STARTED: "not-started",
+  IN_PROGRESS: "in-progress",
+  COMPLETED: "completed",
+  OVERDUE: "overdue",
+}
 
+const taskCategory = {
+  WORK: "work",
+  HOUSEHOLD: "household",
+  ENTERTAINMENT: "entertainment",
+  HOBBY: "hobby",
+}
